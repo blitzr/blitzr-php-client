61d9345f45fe1547f9d44321bd83b5d8a83f2920
Martin Le Guillou <martin.leguillou@y-nov.com>
2016-01-12 17:25:44 +0100

    improve autoloader

---
+++ src/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blitzr;
+
+/**
+ * return client version
+ *
+ * @return string
+ */
+function get_version()
+{
+    return "0.0.1";
+}
---
+++ src/functions_include.php
@@ -0,0 +1,5 @@
+<?php
+// Don't redefine the functions if included multiple times.
+if (!function_exists('Blitzr\get_version')) {
+    require __DIR__ . '/functions.php';
+}
