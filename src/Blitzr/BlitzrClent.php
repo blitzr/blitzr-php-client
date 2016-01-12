e6ccf11f68813aa9103b72778d40ee9ac2349965
Martin Le Guillou <martin.leguillou@y-nov.com>
2016-01-12 16:16:17 +0100

    client creation

---
+++ src/Blitzr/BlitzrClent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Blitzr;
+
+use Blitzr\Exception;
+
+class BlitzrClient
+{
+	private $apiKey;
+
+	function __construct($apiKey)
+	{
+		if ($apiKey === null) {
+			throw new BlitzrConfigurationException('1 est un paramètre invalide', 5);
+		}
+		$this->apiKey = $apiKey;
+	}
+
+	function test()
+	{
+		return $apiKey;
+	}
+}
