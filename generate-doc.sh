a600a5576000f9ff782e4b904c10f8c47c06201d
Martin Le Guillou <martin.leguillou@y-nov.com>
2016-09-30 14:04:49 +0200

    script doc generator

---
+++ generate-doc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./vendor/bin/phpdoc --directory=src/ --target=doc/ --template="vendor/cvuorinen/phpdoc-markdown-public/data/templates/markdown-public" --title="Blitzr PHP client Documentation"
