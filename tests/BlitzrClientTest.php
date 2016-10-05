392834d6f0ec57c455ed6529e477732d62dffc54
Pascal <pascal@blitzr.com>
2016-10-05 14:33:14 +0200

    refactor tests to be compatible with phpunit 4.8

--- tests/BlitzrClientTest.php
+++ tests/BlitzrClientTest.php
@@ -2,18 +2,19 @@
 
 use Blitzr\BlitzrClient;
 
-use Blitzr\Exception\ConfigurationException;
 
 class BlitzrClientTest extends \PHPUnit_Framework_TestCase
 {
 
     const API_KEY = 'API_KEY';
 
+    /**
+     * @expectedException        Blitzr\Exception\ConfigurationException
+     * @expectedExceptionCode 0
+     * @expectedExceptionMessage You must provide an API key.
+     */
     public function testInitWithNullKey()
     {
-        $this->expectException(ConfigurationException::class);
-        $this->expectExceptionCode(0);
-        $this->expectExceptionMessage('You must provide an API key.');
         new BlitzrClient(null);
     }
 }
